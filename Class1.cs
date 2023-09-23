516c344c00dd29a02f35893960618325d54bd58d
Walter Cheng <chengwaltj@gmail.com>
2023-09-22 20:59:48 -0700

    learned how to use revit api

---
+++ Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+
+namespace revit_plugin_1
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
+                Document doc = uidoc.Document;
+                Application app = doc.Application;
+
+                Selection selection = uidoc.Selection;
+                ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
+
+                View src_view = doc.ActiveView;
+
+                FileSaveDialog selectFile = new FileSaveDialog("Revit Files (*.rvt)|*.rvt");
+                selectFile.Show();
+
+                var fileName = ModelPathUtils.ConvertModelPathToUserVisiblePath(selectFile.GetSelectedModelPath());
+
+                Document newDoc = app.NewProjectDocument(UnitSystem.Imperial);
+
+                SaveAsOptions options = new SaveAsOptions();
+                options.OverwriteExistingFile = true;
+
+                newDoc.SaveAs(fileName, options);
+
+                commandData.Application.OpenAndActivateDocument(fileName);
+                newDoc = commandData.Application.ActiveUIDocument.Document;
+
+                Transaction transactionNew = new Transaction(newDoc, "bobr");
+                Transaction transactionOld = new Transaction(doc, "bobr2");
+
+                transactionNew.Start();
+                View dest_view = newDoc.ActiveView;
+                ViewSheet sheet = ViewSheet.Create(newDoc, ElementId.InvalidElementId);
+                transactionNew.Commit();
+
+                foreach (ElementId id in selectedIds)
+                {
+                    transactionOld.Start();
+                    View view = doc.GetElement(id) as ViewDrafting;
+                    transactionOld.Commit();
+
+                    transactionNew.Start();
+                    ICollection<ElementId> dest = ElementTransformUtils.CopyElements(doc, new List<ElementId> { id }, newDoc, Transform.Identity, null);
+                    View destView = newDoc.GetElement(dest.ElementAt(0)) as View;
+                    var parts = new FilteredElementCollector(doc, view.Id);
+                    List<ElementId> items = parts.WhereElementIsNotElementType().ToElementIds().ToList();
+                    items.RemoveAt(0);
+                    foreach (var item in items.ToList())
+                    {
+                        if (doc.GetElement(item).ToString() == "Autodesk.Revit.DB.ReferencePlane") items.Remove(item);
+                    }
+
+                    ElementTransformUtils.CopyElements(view, items, destView, Transform.Identity, null);
+
+                    Viewport viewPort = Viewport.Create(newDoc, sheet.Id, destView.Id, new XYZ());
+
+                    transactionNew.Commit();
+                }
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("revit-plugin-1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("revit-plugin-1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4813add4-c599-49e0-8bdb-ea88e39b3a37")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
